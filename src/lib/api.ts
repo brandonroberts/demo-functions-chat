e98918303cb5d4f75fb38ddc6e133f42dc7dbad3
Brandon <robertsbt@gmail.com>
2022-02-24 14:17:17 -0600

    feat: add Appwrite

---
+++ src/lib/api.ts
@@ -0,0 +1,5 @@
+import { Appwrite } from 'appwrite';
+
+export const api = new Appwrite();
+api.setEndpoint('https://demo.appwrite.io/v1');
+api.setProject('6217c34ce8fef380ed9a');
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,33 @@
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
+
+<div class="p-6 m-4 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">Brandon</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
+
+<div class="p-6 m-4 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">Vincent</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
+
+<div class="p-6 m-4 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+    <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">Wes</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
