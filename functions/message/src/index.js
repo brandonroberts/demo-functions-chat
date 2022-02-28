7b1435f6c0a12a3e6bd91dc0c2f8744548bbdb45
Brandon <robertsbt@gmail.com>
2022-02-28 10:48:14 -0600

    feat: add function to update message with meme

--- functions/message/src/index.js
+++ functions/message/src/index.js
@@ -1,4 +1,13 @@
-const sdk = require("node-appwrite");
+const sdk = require('node-appwrite');
+const fetch = require('node-fetch');
+const { Headers, Request, Response } = require('node-fetch');
+
+if (!globalThis.fetch) {
+	globalThis.fetch = fetch;
+	globalThis.Headers = Headers;
+	globalThis.Request = Request;
+	globalThis.Response = Response;
+}
 
 /*
   'req' variable has:
@@ -14,34 +23,61 @@ const sdk = require("node-appwrite");
 */
 
 module.exports = async function (req, res) {
-  const client = new sdk.Client();
+	const client = new sdk.Client();
 
-  // You can remove services you don't use
-  let account = new sdk.Account(client);
-  let avatars = new sdk.Avatars(client);
-  let database = new sdk.Database(client);
-  let functions = new sdk.Functions(client);
-  let health = new sdk.Health(client);
-  let locale = new sdk.Locale(client);
-  let storage = new sdk.Storage(client);
-  let teams = new sdk.Teams(client);
-  let users = new sdk.Users(client);
+	// You can remove services you don't use
+	let account = new sdk.Account(client);
+	let avatars = new sdk.Avatars(client);
+	let database = new sdk.Database(client);
+	let functions = new sdk.Functions(client);
+	let health = new sdk.Health(client);
+	let locale = new sdk.Locale(client);
+	let storage = new sdk.Storage(client);
+	let teams = new sdk.Teams(client);
+	let users = new sdk.Users(client);
 
-  if (
-    !req.env['APPWRITE_FUNCTION_ENDPOINT'] ||
-    !req.env['APPWRITE_FUNCTION_API_KEY']
-  ) {
-    console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
-  } else {
-    client
-      .setEndpoint(req.env['APPWRITE_FUNCTION_ENDPOINT'])
-      .setProject(req.env['APPWRITE_FUNCTION_PROJECT_ID'])
-      .setKey(req.env['APPWRITE_FUNCTION_API_KEY'])
-      .setSelfSigned(true);
-  }
+	if (!req.env['APPWRITE_FUNCTION_ENDPOINT'] || !req.env['APPWRITE_FUNCTION_API_KEY']) {
+		console.warn('Environment variables are not set. Function cannot use Appwrite SDK.');
+	} else {
+		client
+			.setEndpoint(req.env['APPWRITE_FUNCTION_ENDPOINT'])
+			.setProject(req.env['APPWRITE_FUNCTION_PROJECT_ID'])
+			.setKey(req.env['APPWRITE_FUNCTION_API_KEY'])
+			.setSelfSigned(true);
+	}
 
-  res.json({
-    areDevelopersAwesome: true,
-    eventData: req.env['APPWRITE_FUNCTION_EVENT_DATA']
-  });
+	const { GiphyFetch } = require('@giphy/js-fetch-api');
+
+	// use @giphy/js-fetch-api to fetch gifs, instantiate with your api key
+	const gf = new GiphyFetch(req.env['GIPHY_API_KEY']);
+
+	// parse event data
+	const data = JSON.parse(req.env['APPWRITE_FUNCTION_EVENT_DATA']);
+
+	// fetch gif based on message
+	const gifs = await gf.search(data.message, { limit: 1 });
+
+	const resp = {
+		areDevelopersAwesome: true,
+		eventData: data,
+		giphyKey: req.env['GIPHY_API_KEY'],
+		gif: gifs.data[0] ? gifs.data[0].images.downsized.url : ''
+	};
+
+	const collectionId = data.$collection;
+	const documentId = data.$id;
+	const { user, room, message, $read, $write } = data;
+	const document = {
+		user,
+		room,
+		message,
+		meme: resp.gif
+	};
+
+	try {
+		await database.updateDocument(collectionId, documentId, document, $read, $write);
+		res.json({ success: true });
+	} catch (e) {
+		res.send(`Unable to update meme for message ${e}}`, 400);
+	}
 };
